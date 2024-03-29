55544be700f187b51cbbc869c0d0ba8a61ccc767
tangbo <1430505705@qq.com>
2024-03-29 22:36:25 +0800

    keep day 33

---
+++ 面试经典 150 题/二叉树/相同的树.go
@@ -0,0 +1,17 @@
+package 二叉树
+
+// 思路：深度优先遍历，两边节点一模一样就是相同的树
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
