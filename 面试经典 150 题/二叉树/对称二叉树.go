b46d6902ccee8bbc7a26e5d2bf683dcc237313dd
tangbo <1430505705@qq.com>
2024-03-31 23:16:45 +0800

    keep day 35

---
+++ 面试经典 150 题/二叉树/对称二叉树.go
@@ -0,0 +1,18 @@
+package 二叉树
+
+// 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+
+func isSymmetric(root *TreeNode) bool {
+	return check(root.Left, root.Right)
+}
+
+func check(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+
+	return p.Val == q.Val && check(p.Left, p.Right) && check(p.Left, p.Right)
+}
