d6f1309a87cc509672df01be942e90d816fcf3cc
Bo.Tang <1430505705@qq.com>
2024-04-08 20:52:48 +0800

    tmp

---
+++ 面试经典 150 题/二叉树/二叉树的锯齿形层序遍历.go
@@ -0,0 +1,42 @@
+package 二叉树
+
+// 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。
+//（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	index := 0
+	res := make([][]int, 0)
+	out := []*TreeNode{root}
+	for len(out) > 0 {
+		tmp := make([]int, 0)
+		res = append(res, []int{})
+		in := []*TreeNode{}
+		for i := 0; i < len(out); i++ {
+			node := out[i]
+			if index%2 == 0 {
+				res[index] = append(res[index], node.Val)
+			} else {
+				tmp = append(tmp, node.Val)
+			}
+
+			if node.Right != nil {
+				in = append(in, node.Right)
+			}
+
+			if node.Left != nil {
+				in = append(in, node.Left)
+			}
+		}
+		for len(tmp) > 0 {
+			res[index] = append(res[index], tmp[len(tmp)-1])
+			tmp = tmp[:len(tmp)-1]
+		}
+
+		out = in
+		index++
+	}
+	return res
+}
