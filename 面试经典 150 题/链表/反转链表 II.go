b8248cba8ad8249c43c03732e0f4ea278a59a101
tangbo <1430505705@qq.com>
2024-04-09 22:02:41 +0800

    keep day 40

--- 面试经典 150 题/链表/反转链表 II.go
+++ 面试经典 150 题/链表/反转链表 II.go
@@ -2,6 +2,7 @@ package 链表
 
 // 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
 
+// 先走到要翻转的起点，然后取一个当前节点与当前节点的前一个节点，然后进行翻转就行
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
 	ret := &ListNode{Val: -1}
 	ret.Next = head
